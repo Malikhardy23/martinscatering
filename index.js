6afb6448a202cc2f822864603d21ebe87e3df557
Malik Hardy <50418576+Malikhardy23@users.noreply.github.com>
2022-04-24 11:13:53 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+// sticky navbar //
+
+var topSection = document.getElementById("topSection");
+var menu = document.getElementById("menu");
+
+window.onscroll = function(){
+    if(window.pageYOffset >= menu.offsetTop){
+        topSection.classList.add("sticky");
+    }
+    else{
+        topSection.classList.remove("sticky")
+    }
+}
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+
